48d26bb1f28f28756e5725298c57538076090ade
Your Name <you@example.com>
2022-12-23 13:45:29 +0500

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,13 @@ import { IoLanguageSharp } from 'react-icons/io5'
 import { FaDraftingCompass } from 'react-icons/fa'
 import { MdOutlineComputer } from 'react-icons/md'
 import { GiSecretBook } from 'react-icons/gi'
+import avatar from './avatar.png'
 import img_1 from '../components/img_1.png'
+import vector from './Vector.png'
+import coins from './coins 1.png'
+import star from './star.png'
+import users from './users.png'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -87,11 +93,138 @@ const Home = () => {
           <h3 className='text-[45px] font-medium text-[#37363C] tracking-[2px]'>Bizning TOP o'qituvchilarimiz</h3>
           <p className='mb-6 font-[400] text-[#37363c] text-[18px]'>Hoziroq o'zingizga ma'qul ustozni tanlang va o'rganishni boshlang.  MyTeacher sizga.O'zbekistonning barchaburchagidan eng kuchli va saralangan
             ustozlarni topadi va ushbu ro'yhatda ko'rsatadi. DarTslar individual tarzdao'tiladi, har bir dars 1 soatdan davom etadi.</p>
-          
-          <div className='space-y-6'>
-            <div className='flex items-center justify-center space-x-6'>
-              <div className='w-[600px] h-[264px] bg-[#F5F5F7] rounded-[8px] border border-[rgba(55, 54, 60, 0.1)]'>
 
+          <div className='space-y-8'>
+            <div className='flex items-center justify-center space-x-8'>
+              <div className='px-5 py-7 w-[49%] flex items-center justify-center space-x-5 h-[280px] bg-[#F5F5F7] rounded-[8px] border border-[rgba(55, 54, 60, 0.1)]'>
+                <img src={avatar} alt="" />
+                <div className='space-y-2'>
+                  <h3 className='font-bold text-[#37363C] text-[25px]'>Abdujalol Xalimov</h3>
+                  <div className='flex items-center'>
+                    <img src={vector} alt="" />
+                    <h4 className='ml-1 font-medium text-[#37363C] text-[17px]'>Matematika</h4>
+                  </div>
+                  <div className='space-x-4 flex items-center'>
+                    <div className='flex items-center'>
+                      <img src={coins} alt="" />
+                      <p className='text-[#37363C] text-[20px]'><span className='text-[#A30041] font-medium text-[24px]'>45 000</span>/soat</p>
+                    </div>
+                    <div className='flex space-x-1 items-center'>
+                      <img src={star} alt="" />
+                      <p className='font-medium text-[24px] text-[#A30041]'>5</p>
+                      <p className='text-[#37363C] text-[16px] mt-[5.5px]'><span className='text-[#0DAD13]'>16 ta</span> sharx</p>
+                    </div>
+                  </div>
+                  <div className='flex items-center space-x-1'>
+                    <img src={users} alt="" />
+                    <p className='text-[#37363C] mt-[6px] tetx-[16px]'><span className='text-[#0DAD13]'>12 ta</span> talaba</p>
+                  </div>
+                  <p className='text-[16px] text-[#37363C]'>
+                    Tajriba: <span className='text-[#0DAD13]'>1 yil</span>,   Daraja: <span className='text-[#0DAD13]'>C1</span>,   Sertifikat: <span className='text-[#0DAD13]'>IELTS-6.5</span>,
+                    Qaysi tillarni biladi: <span className='text-[#0DAD13]'>Rus tili, Ingliz tili, Tojik tili.</span>
+                  </p>
+                  <div className='mt-2'>
+                    <Link href='/' className='text-[18px] text-[#0DAD13]'>Batafsil</Link>
+                  </div>
+                </div>
+              </div>
+              <div className='px-5 py-7 w-[49%] flex items-center justify-center space-x-5 h-[280px] bg-[#F5F5F7] rounded-[8px] border border-[rgba(55, 54, 60, 0.1)]'>
+                <img src={avatar} alt="" />
+                <div className='space-y-2'>
+                  <h3 className='font-bold text-[#37363C] text-[25px]'>Abdujalol Xalimov</h3>
+                  <div className='flex items-center'>
+                    <img src={vector} alt="" />
+                    <h4 className='ml-1 font-medium text-[#37363C] text-[17px]'>Matematika</h4>
+                  </div>
+                  <div className='space-x-4 flex items-center'>
+                    <div className='flex items-center'>
+                      <img src={coins} alt="" />
+                      <p className='text-[#37363C] text-[20px]'><span className='text-[#A30041] font-medium text-[24px]'>45 000</span>/soat</p>
+                    </div>
+                    <div className='flex space-x-1 items-center'>
+                      <img src={star} alt="" />
+                      <p className='font-medium text-[24px] text-[#A30041]'>5</p>
+                      <p className='text-[#37363C] text-[16px] mt-[5.5px]'><span className='text-[#0DAD13]'>16 ta</span> sharx</p>
+                    </div>
+                  </div>
+                  <div className='flex items-center space-x-1'>
+                    <img src={users} alt="" />
+                    <p className='text-[#37363C] mt-[6px] tetx-[16px]'><span className='text-[#0DAD13]'>12 ta</span> talaba</p>
+                  </div>
+                  <p className='text-[16px] text-[#37363C]'>
+                    Tajriba: <span className='text-[#0DAD13]'>1 yil</span>,   Daraja: <span className='text-[#0DAD13]'>C1</span>,   Sertifikat: <span className='text-[#0DAD13]'>IELTS-6.5</span>,
+                    Qaysi tillarni biladi: <span className='text-[#0DAD13]'>Rus tili, Ingliz tili, Tojik tili.</span>
+                  </p>
+                  <div className='mt-2'>
+                    <Link href='/' className='text-[18px] text-[#0DAD13]'>Batafsil</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='flex items-center justify-center space-x-8'>
+              <div className='px-5 py-7 w-[49%] flex items-center justify-center space-x-5 h-[280px] bg-[#F5F5F7] rounded-[8px] border border-[rgba(55, 54, 60, 0.1)]'>
+                <img src={avatar} alt="" />
+                <div className='space-y-2'>
+                  <h3 className='font-bold text-[#37363C] text-[25px]'>Abdujalol Xalimov</h3>
+                  <div className='flex items-center'>
+                    <img src={vector} alt="" />
+                    <h4 className='ml-1 font-medium text-[#37363C] text-[17px]'>Matematika</h4>
+                  </div>
+                  <div className='space-x-4 flex items-center'>
+                    <div className='flex items-center'>
+                      <img src={coins} alt="" />
+                      <p className='text-[#37363C] text-[20px]'><span className='text-[#A30041] font-medium text-[24px]'>45 000</span>/soat</p>
+                    </div>
+                    <div className='flex space-x-1 items-center'>
+                      <img src={star} alt="" />
+                      <p className='font-medium text-[24px] text-[#A30041]'>5</p>
+                      <p className='text-[#37363C] text-[16px] mt-[5.5px]'><span className='text-[#0DAD13]'>16 ta</span> sharx</p>
+                    </div>
+                  </div>
+                  <div className='flex items-center space-x-1'>
+                    <img src={users} alt="" />
+                    <p className='text-[#37363C] mt-[6px] tetx-[16px]'><span className='text-[#0DAD13]'>12 ta</span> talaba</p>
+                  </div>
+                  <p className='text-[16px] text-[#37363C]'>
+                    Tajriba: <span className='text-[#0DAD13]'>1 yil</span>,   Daraja: <span className='text-[#0DAD13]'>C1</span>,   Sertifikat: <span className='text-[#0DAD13]'>IELTS-6.5</span>,
+                    Qaysi tillarni biladi: <span className='text-[#0DAD13]'>Rus tili, Ingliz tili, Tojik tili.</span>
+                  </p>
+                  <div className='mt-2'>
+                    <Link href='/' className='text-[18px] text-[#0DAD13]'>Batafsil</Link>
+                  </div>
+                </div>
+              </div>
+              <div className='px-5 py-7 w-[49%] flex items-center justify-center space-x-5 h-[280px] bg-[#F5F5F7] rounded-[8px] border border-[rgba(55, 54, 60, 0.1)]'>
+                <img src={avatar} alt="" />
+                <div className='space-y-2'>
+                  <h3 className='font-bold text-[#37363C] text-[25px]'>Abdujalol Xalimov</h3>
+                  <div className='flex items-center'>
+                    <img src={vector} alt="" />
+                    <h4 className='ml-1 font-medium text-[#37363C] text-[17px]'>Matematika</h4>
+                  </div>
+                  <div className='space-x-4 flex items-center'>
+                    <div className='flex items-center'>
+                      <img src={coins} alt="" />
+                      <p className='text-[#37363C] text-[20px]'><span className='text-[#A30041] font-medium text-[24px]'>45 000</span>/soat</p>
+                    </div>
+                    <div className='flex space-x-1 items-center'>
+                      <img src={star} alt="" />
+                      <p className='font-medium text-[24px] text-[#A30041]'>5</p>
+                      <p className='text-[#37363C] text-[16px] mt-[5.5px]'><span className='text-[#0DAD13]'>16 ta</span> sharx</p>
+                    </div>
+                  </div>
+                  <div className='flex items-center space-x-1'>
+                    <img src={users} alt="" />
+                    <p className='text-[#37363C] mt-[6px] tetx-[16px]'><span className='text-[#0DAD13]'>12 ta</span> talaba</p>
+                  </div>
+                  <p className='text-[16px] text-[#37363C]'>
+                    Tajriba: <span className='text-[#0DAD13]'>1 yil</span>,   Daraja: <span className='text-[#0DAD13]'>C1</span>,   Sertifikat: <span className='text-[#0DAD13]'>IELTS-6.5</span>,
+                    Qaysi tillarni biladi: <span className='text-[#0DAD13]'>Rus tili, Ingliz tili, Tojik tili.</span>
+                  </p>
+                  <div className='mt-2'>
+                    <Link href='/' className='text-[18px] text-[#0DAD13]'>Batafsil</Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
